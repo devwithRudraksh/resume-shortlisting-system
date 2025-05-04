1b27586503d4a826e794c7f517c2a9ac9d981e5e
devwithrudraksh <rudrakshsahoo@gmail.com>
2025-05-04 15:28:13 +0530

    Initial clean Spring Boot setup with MySQL Docker and application.yml

---
+++ shortlistingsystem/src/main/java/com/resume/example/shortlistingsystem/ShortlistingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.resume.example.shortlistingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortlistingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortlistingSystemApplication.class, args);
+	}
+
+}
---
+++ shortlistingsystem/src/test/java/com/resume/example/shortlistingsystem/ShortlistingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume.example.shortlistingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlistingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
